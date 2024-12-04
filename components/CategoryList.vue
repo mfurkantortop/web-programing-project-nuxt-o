41191dcd7c31bb1ee9739a1abbc783733b41e8fa
mfurkantortop <mfurkantortop@gmail.com>
2024-12-04 10:53:38 +0300

    CategoryList

---
+++ components/CategoryList.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul class="flex gap-4">
+      <li v-for="category in categories" :key="category" class="bg-gray-100 p-2 rounded-md shadow">
+        {{ category }}
+      </li>
+    </ul>
+  </template>
+  
+  <script lang="ts" setup>
+  defineProps({
+    categories: Array as () => string[],
+  });
+  </script>
+  
+  <style scoped>
+  </style>
+  

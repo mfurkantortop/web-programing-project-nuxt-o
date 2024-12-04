c00392b69a5ba9894062745abeb9bacd6635e9ca
mfurkantortop <mfurkantortop@gmail.com>
2024-12-04 10:52:21 +0300

    Testimonial

---
+++ components/Testimonial.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="border p-4 rounded-lg shadow-md">
+      <p class="italic">"{{ message }}"</p>
+      <p class="text-right font-semibold">- {{ name }}</p>
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  defineProps({
+    message: String,
+    name: String,
+  });
+  </script>
+  
+  <style scoped>
+  </style>
+  

d32fd7130ac74a8247e5c65bf49f6c52e0532e0b
mfurkantortop <mfurkantortop@gmail.com>
2024-12-04 10:54:14 +0300

    Footer

---
+++ components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer class="bg-gray-800 text-white p-4 text-center">
+      <p>&copy; 2024 Konfuse Shop. All rights reserved.</p>
+    </footer>
+  </template>
+  
+  <script lang="ts" setup>
+  </script>
+  
+  <style scoped>
+  </style>
+  

ea4866f11e039a7eaced56207e62f3164c7de323
mfurkantortop <mfurkantortop@gmail.com>
2024-12-04 10:49:58 +0300

    ContactForm

---
+++ components/ContactForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <form class="grid gap-4">
+      <input type="text" placeholder="Your Name" class="border p-2 rounded-md" />
+      <input type="email" placeholder="Your Email" class="border p-2 rounded-md" />
+      <textarea placeholder="Your Message" class="border p-2 rounded-md"></textarea>
+      <button class="bg-blue-600 text-white px-4 py-2 rounded-md">Send</button>
+    </form>
+  </template>
+  
+  <script lang="ts" setup>
+  </script>
+  
+  <style scoped>
+  </style>
+  

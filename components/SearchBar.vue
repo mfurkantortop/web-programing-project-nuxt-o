d8976e8ab61f1b9de106cf254b73593245eba273
mfurkantortop <mfurkantortop@gmail.com>
2024-12-04 10:52:59 +0300

    SearchBar

---
+++ components/SearchBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex items-center border rounded-lg p-2">
+      <input
+        type="text"
+        placeholder="Search for products..."
+        class="flex-1 p-2 outline-none"
+      />
+      <button class="bg-blue-600 text-white px-4 py-2 rounded-md">Search</button>
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  </script>
+  
+  <style scoped>
+  </style>
+  

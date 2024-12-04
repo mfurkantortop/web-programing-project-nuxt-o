98ce056aadfdc281ce5eac92d6a5363182498dae
mfurkantortop <mfurkantortop@gmail.com>
2024-12-04 10:54:45 +0300

    ProductCard

---
+++ components/ProductCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="border rounded-lg shadow-md p-4">
+      <img :src="image" alt="Product Image" class="w-full h-40 object-cover rounded-md mb-4" />
+      <h3 class="text-lg font-semibold mb-2">{{ title }}</h3>
+      <p class="text-gray-700 mb-2">{{ description }}</p>
+      <span class="text-green-600 font-bold">{{ price }}</span>
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  defineProps({
+    image: String,
+    title: String,
+    description: String,
+    price: String,
+  });
+  </script>
+  
+  <style scoped>
+  </style>
+  

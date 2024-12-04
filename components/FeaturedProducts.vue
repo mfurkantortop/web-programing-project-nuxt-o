2388fb0ee20f7051b92eb020c41f524b1cb6ec41
mfurkantortop <mfurkantortop@gmail.com>
2024-12-04 10:51:03 +0300

    FeaturedProducts

---
+++ components/FeaturedProducts.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+      <h2 class="text-2xl font-bold mb-4">Featured Products</h2>
+      <div class="grid grid-cols-2 gap-4">
+        <ProductCard
+          v-for="product in products"
+          :key="product.id"
+          :image="product.image"
+          :title="product.title"
+          :description="product.description"
+          :price="product.price"
+        />
+      </div>
+    </section>
+  </template>
+  
+  <script lang="ts" setup>
+  import ProductCard from './ProductCard.vue';
+  
+  defineProps({
+    products: Array as () => {
+      id: number;
+      image: string;
+      title: string;
+      description: string;
+      price: string;
+    }[],
+  });
+  </script>
+  
+  <style scoped>
+  </style>
+  

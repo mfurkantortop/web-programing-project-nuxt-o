a4ca29700488db66c3900f0622bc39b40c8166f0
mfurkantortop <mfurkantortop@gmail.com>
2024-12-04 10:48:29 +0300

    basic things for nuxt

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <Header />
+      <HeroSection />
+      <SearchBar />
+      <FeaturedProducts :products="products" />
+      <CategoryList :categories="categories" />
+      <Testimonial
+        message=""
+        name=""
+      />
+      <NewsletterSignup />
+      <Footer />
+    </div>
+  </template>
